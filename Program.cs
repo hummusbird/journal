43991ae0d5fbc0eb709229fd02038bd367e793b8
hummusbird <birb@birb.cc>
2024-01-22 11:36:15 +0000

    setup web application

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace journal;
+﻿using Microsoft.AspNetCore.HttpOverrides;
 
-class Program
+string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
+WebApplication app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.UseHttpsRedirection();  // redirect 80 to 443
+app.UseDefaultFiles();      // use index.html & index.css
+app.UseStaticFiles();       // enable static file serving
+app.UseCors(MyAllowSpecificOrigins);
+
+app.Run();
