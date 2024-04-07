f2b2a81743e937a0d684565a50d588435df6a339
hummusbird <birb@birb.cc>
2024-04-07 10:41:16 +0100

    fix warnings

--- journal.cs
+++ journal.cs
@@ -1,6 +1,6 @@
 public class Journal
 {
-    public static List<string> journal;
+    public static List<string>? journal;
 
     public static void Load()
     {
@@ -10,12 +10,12 @@ public class Journal
 
     public static string ReadDate(string date)
     { // todo: return between 3am and 3am the next day, not the whole day
-        return string.Join("\n", journal.Where(line => line.StartsWith("[" + date)).ToList());
+        return string.Join("\n", journal!.Where(line => line.StartsWith("[" + date)).ToList());
     }
 
     public static void AddEntry(string entry)
     {
         string timestamp = "[" + DateTime.Now.ToString("yyyy-MM-dd hh:mmtt") + "] ";
-        journal.Add(timestamp + entry);
+        journal!.Add(timestamp + entry);
     }
 }
