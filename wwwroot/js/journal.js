1ec4803f30652d4603582bde73c3f48c60e697b7
hummusbird <birb@birb.cc>
2024-04-16 15:39:43 +0100

    journal: show today in textarea

--- wwwroot/js/journal.js
+++ wwwroot/js/journal.js
@@ -3,5 +3,6 @@ const url = window.location.host
 async function GetJournal() {
     let res = await fetch("/api/journal/today")
     let text = await res.text();
+    document.getElementById("textzone").value = text;
     console.log(text)
 }
