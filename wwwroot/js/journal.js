34152bd68b5d53a11279fa13d7c3b00dfcf9caca
hummusbird <birb@birb.cc>
2024-04-17 10:31:05 +0100

    journal.js: SendEntry();

--- wwwroot/js/journal.js
+++ wwwroot/js/journal.js
@@ -1,8 +1,23 @@
 const url = window.location.host
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function GetJournal() {
     let res = await fetch("/api/journal/today")
     let text = await res.text();
     document.getElementById("textzone").value = text;
     console.log(text)
+}
+
+function SendEntry() {
+    let formData = new FormData();
+    formData.append("data", document.getElementById("entrybox").value)
+
+    fetch("/api/journal/new",
+        {
+            body: formData,
+            method: "POST"
+        })
+        .catch(e => console.log(e))
+    delay(50);
+    GetJournal();
 }
